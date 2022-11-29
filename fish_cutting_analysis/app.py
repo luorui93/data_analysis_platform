49d8871b57e4f273f9390c497a8b83a489ba79d8
Rui Luo <luo.rui@husky.neu.edu>
2022-11-28 20:41:13 -0500

    Supported selecting data folder using GUI Added hammer sync support

--- fish_cutting_analysis/app.py
+++ fish_cutting_analysis/app.py
@@ -10,21 +10,23 @@ from skimage.transform import rescale
 import scipy.io
 import numpy as np
 import pandas as pd
+import glob
+import os
 
 from rich.console import Console
-import os.path
-import time
+
+from tkinter import filedialog
+from tkinter import *
 
 console = Console()
-hololens_folder = "/trial1/hololens_camera-rgb-image_raw-compressed/"
-camera1_folder = "/trial1/cam_1-color-image_raw/"
-camera2_folder = "/trial1/cam_2-rgb-image_rect_color/"
-trial1_data = "../trial1/Trial1_Cut_Fillets_Move_Toss.mat"
+hololens_folder = "/hololens_camera-rgb-image_raw-compressed/"
+camera1_folder = "/cam_1-color-image_raw/"
+camera2_folder = "/cam_2-rgb-image_rect_color/"
 mocap_start_time = 0
-mocap_id = 1
+p1_mocap_id = 3
 video_offset = 8
 
-# Mocap label reference: 
+# Mocap label reference:
 # ['Hammer' 'Right Elbow' 'Left Elbow' 'Right Shoulder' 'Left Shoulder'
 #  'Right Wrist Pinky' 'Right Wrist Thumb' 'Right Pinky' 'Right Index'
 #  'Right Thumb' 'Left Wrist Pinky' 'Left Wrist Thumb' 'Left Thumb'
@@ -38,7 +40,9 @@ colors = {
 
 class DataProcessServer(object):
     def __init__(self):
-        self.parent_dir = os.path.abspath("..")
+        Tk().withdraw()
+        self.parent_dir = filedialog.askdirectory()
+
         self.frame_idx = 0
         self.read_frames()
         self.load_mocap_mat()
@@ -47,27 +51,40 @@ class DataProcessServer(object):
         self.app.layout = self.generate_layout()
         self.register_callbacks()
 
+        self.hololens_hammer_point = 0
+        self.camera1_hammer_point = 0
+        self.camera2_hammer_point = 0
+        self.mocap_hammer_point = 0
+
     def read_frames(self):
         """Read frame collection from path"""
         print(self.parent_dir + hololens_folder)
-        self.hololens_frames = io.imread_collection(self.parent_dir + hololens_folder + "*.jpg")
+        self.hololens_frames = io.imread_collection(
+            self.parent_dir + hololens_folder + "*.jpg")
         console.print(f'Read {len(self.hololens_frames)} hololens frames')
-        self.cam1_frames = io.imread_collection(self.parent_dir + camera1_folder + "*.jpg")
+        self.cam1_frames = io.imread_collection(
+            self.parent_dir + camera1_folder + "*.jpg")
         console.print(f'Read {len(self.cam1_frames)} camera1 frames')
-        self.cam2_frames = io.imread_collection(self.parent_dir + camera2_folder + "*.jpg")
+        self.cam2_frames = io.imread_collection(
+            self.parent_dir + camera2_folder + "*.jpg")
         console.print(f'Read {len(self.cam2_frames)} camera2 frames')
 
     def load_mocap_mat(self):
-        self.p1_mat = scipy.io.loadmat(trial1_data, simplify_cells=True)
+        # return the first found mat file
+        mat_path = glob.glob(self.parent_dir+"/*.mat")[0]
+        mocap_name = os.path.splitext(os.path.basename(mat_path))[0]
+        self.entry_name = mocap_name
+        self.p1_mat = scipy.io.loadmat(mat_path, simplify_cells=True)
+        console.print(f"Read mocap data from {mat_path}")
         console.print("Mocap labels")
-        console.print(self.p1_mat['Cut_Fillets_Move_Toss']
+        console.print(self.p1_mat[mocap_name]
                       ['Trajectories']['Labeled']['Labels'])
-        self.p1_label = (self.p1_mat['Cut_Fillets_Move_Toss']
-                      ['Trajectories']['Labeled']['Labels'])
-        console.print(self.p1_mat['Cut_Fillets_Move_Toss']
-                      ['Trajectories']['Labeled']['Data'][mocap_id, :, :].T.shape)
-        self.p1_df = pd.DataFrame(self.p1_mat['Cut_Fillets_Move_Toss']['Trajectories']
-                                  ['Labeled']['Data'][mocap_id, 0:3, mocap_start_time:].T, columns=['x', 'y', 'z'])
+        self.p1_label = (self.p1_mat[mocap_name]
+                         ['Trajectories']['Labeled']['Labels'])
+        console.print(self.p1_mat[mocap_name]
+                      ['Trajectories']['Labeled']['Data'][p1_mocap_id, :, :].T.shape)
+        self.p1_df = pd.DataFrame(self.p1_mat[mocap_name]['Trajectories']
+                                  ['Labeled']['Data'][p1_mocap_id, 0:3, mocap_start_time:].T, columns=['x', 'y', 'z'])
 
     def update_text(self, n):
         return [html.Span('Test'+str(n))]
@@ -88,12 +105,23 @@ class DataProcessServer(object):
             Input('slider-frame', 'value')
         )(self.update_plots)
 
+        self.app.callback(
+            Output('sync-time', "value"),
+            Input('hololens-hammer-frame', "value"),
+            Input('camera1-hammer-frame', "value"),
+            Input('camera2-hammer-frame', "value"),
+            Input('mocap-hammer-frame', "value")
+        )(self.sync_hammer)
+
     def generate_layout(self):
         """Generate and return app components layout"""
         return html.Div(
             [
-                dbc.Row(dbc.Col(html.H1("Fish Cutting Data Analysis", className="btn-primary"))),
-                dbc.Row(dbc.Col(html.Br())),
+                dbc.Row(
+                    dbc.Col(html.H1("Fish Cutting Data Analysis", className="btn-primary"))),
+                dbc.Row(dbc.Col(html.H2(self.entry_name,
+                                        className="btn-primary"), width=2)),
+                dbc.Row(dbc.Col(html.Br(id="sync-time"))),
                 dbc.Row([
                         dbc.Col(html.Div([
                             html.H3("Hololens", className="btn-secondary"),
@@ -109,12 +137,30 @@ class DataProcessServer(object):
                         ]))
                         ]),
                 dbc.Row([
+                        dbc.Col(html.H3("Hololens hammer point: ", className="btn-secondary"), width=2),
+                        dbc.Col(dcc.Input(id="hololens-hammer-frame", type="number", value=0,
+                                           debounce=True, placeholder="Hammer Frame"), width=2),
+                        dbc.Col(html.H3("Camera1 hammer point: ", className="btn-secondary"), width=2),
+                        dbc.Col(dcc.Input(id="camera1-hammer-frame", type="number", value=0,
+                                           debounce=True, placeholder="Hammer Frame"), width=2),
+                        dbc.Col(html.H3("Camera2 hammer point: ", className="btn-secondary"), width=2),
+                        dbc.Col(dcc.Input(id="camera2-hammer-frame", type="number", value=0,
+                                           debounce=True, placeholder="Hammer Frame"), width=2)]),
+                dbc.Row([
                     dbc.Col([
-                        html.H2(f"Tracker {self.p1_label[mocap_id]}", className="btn-warning"),
+                        html.H2(
+                            f"Tracker {self.p1_label[p1_mocap_id]}", className="btn-warning"),
                         dcc.Graph(id='plot1-graph',
                                   figure=px.line(self.p1_df, labels={'index': 'time (10ms)', 'value': 'pos'}, range_x=[0, 3000]))
                     ], width=4),
                 ]),
+
+                dbc.Row([dbc.Col(html.H3("Mocap hammer point: ", className="btn-warning"), width=2),
+                         dbc.Col(dcc.Input(id="mocap-hammer-frame", type="number", value=0,
+                                           debounce=True, placeholder="Hammer Frame"), width=1)]),
+                dbc.Row(dbc.Col(html.Br())),
+                dbc.Row(dbc.Col(html.Br())),
+
                 dbc.Row([
                     dbc.Col(html.Button('Next frame', id='btn-next',
                                         className='btn btn-primary', n_clicks=0), width=1),
@@ -144,21 +190,33 @@ class DataProcessServer(object):
         # elif "interval" == ctx.triggered_id:
         #     self.frame_idx = n_intervals
         fig1 = px.imshow(
-            self.hololens_frames[self.frame_idx + video_offset], binary_format="jpg")
+            self.hololens_frames[self.frame_idx  + self.hololens_hammer_point], binary_format="jpg")
         fig2 = px.imshow(
-            self.cam1_frames[self.frame_idx + video_offset], binary_format="jpg")
+            self.cam1_frames[self.frame_idx + self.camera1_hammer_point], binary_format="jpg")
         fig3 = px.imshow(
-            self.cam2_frames[self.frame_idx*2], binary_format="jpg")
+            self.cam2_frames[(self.frame_idx + self.camera2_hammer_point)*2], binary_format="jpg")
         return fig1, fig2, fig3
 
     def update_plots(self, slider_value):
-        fig = px.line(self.p1_df[0:(slider_value+1)*6],
+        fig = px.line(self.p1_df[self.mocap_hammer_point:(slider_value+1)*6 + self.mocap_hammer_point],
                       labels={'index': 'time (10ms)', 'value': 'pos'}, range_x=[0, 3000])
         return fig
+    
+    def sync_hammer(self, hololens_hammer_point, camera1_hammer_point, camera2_hammer_point, mocap_hammer_point):
+        print("hololens_hammer_point", hololens_hammer_point)
+        print("camera1_hammer_point", camera1_hammer_point)
+        print("camera2_hammer_point", camera2_hammer_point)
+        print("mocap_hammer_point", mocap_hammer_point)
+        self.hololens_hammer_point = hololens_hammer_point
+        self.camera1_hammer_point = camera1_hammer_point
+        self.camera2_hammer_point = camera2_hammer_point 
+        self.mocap_hammer_point = mocap_hammer_point
+        return 0
 
     def run(self):
         console.print("Dash server started")
-        self.app.run_server(debug=True)
+        self.app.run_server(debug=False)
+
 
 if __name__ == '__main__':
     dps = DataProcessServer()
