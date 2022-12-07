31a1ce34a4e03a82c9967bcbf2b6731feaf6f0e2
Rui Luo <luo.rui@husky.neu.edu>
2022-12-06 19:13:43 -0500

    Now use timestamp to synchronize multiple sources instead of using frame indices

--- fish_cutting_analysis/app.py
+++ fish_cutting_analysis/app.py
@@ -35,6 +35,55 @@ colors = {
 }
 
 
+class VideoData(object):
+    def __init__(self, data_folder):
+        self.frames = io.imread_collection(data_folder + "*.jpg")
+        self.timestamp = sorted([int(name[:-4]) for name in
+                                 os.listdir(data_folder) if "jpg" in name])
+        # df is the average time difference between each frame in ms
+        self.df = (self.timestamp[-1] -
+                   self.timestamp[1]) / (len(self.frames)-1)
+        if len(self.timestamp) != len(self.frames):
+            raise ValueError(
+                "The size of timestamp is not equal to the size of frames")
+
+    def get_index(self, time: int):
+        """
+        Return the closet frame index from timestamp string
+
+        :param time: The timestamp integer in milliseconds
+        :return: The index of the frame
+        """
+        est_idx = int(time // self.df)
+        # print("est_idx: ", est_idx)
+        res_idx = est_idx
+        err = abs(self.timestamp[est_idx] - time)
+        if est_idx >= len(self.frames):
+            return len(self.frames) - 1
+        else:
+            next = True
+            prev = True
+            offset = 1
+            while(est_idx < len(self.frames)):
+                if next:
+                    if err > abs(self.timestamp[est_idx+offset] - time):
+                        err = abs(self.timestamp[est_idx+offset] - time)
+                        res_idx = est_idx+offset
+                    else:
+                        next = False
+                if prev:
+                    if err > abs(self.timestamp[est_idx-offset] - time):
+                        err = abs(self.timestamp[est_idx-offset] - time)
+                        res_idx = est_idx-offset
+                    else:
+                        prev = False
+                if next or prev:
+                    offset += 1
+                else:
+                    break
+            return res_idx
+
+
 class DataProcessServer(object):
     def __init__(self):
         Tk().withdraw()
@@ -42,6 +91,7 @@ class DataProcessServer(object):
 
         self.frame_idx = 0
         self.read_frames()
+        # print("Test index: ", self.cam2_data.get_index(25555))
         self.load_mocap_mat()
 
         self.app = Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
@@ -55,16 +105,18 @@ class DataProcessServer(object):
 
     def read_frames(self):
         """Read frame collection from path"""
-        print(self.parent_dir + hololens_folder)
-        self.hololens_frames = io.imread_collection(
-            self.parent_dir + hololens_folder + "*.jpg")
-        console.print(f'Read {len(self.hololens_frames)} hololens frames')
-        self.cam1_frames = io.imread_collection(
-            self.parent_dir + camera1_folder + "*.jpg")
-        console.print(f'Read {len(self.cam1_frames)} camera1 frames')
-        self.cam2_frames = io.imread_collection(
-            self.parent_dir + camera2_folder + "*.jpg")
-        console.print(f'Read {len(self.cam2_frames)} camera2 frames')
+        console.print("Read video data from " + self.parent_dir)
+        self.hololens_data = VideoData(self.parent_dir + hololens_folder)
+        console.print(f'Read {len(self.hololens_data.frames)} hololens frames')
+        print(self.hololens_data.timestamp)
+
+        self.cam1_data = VideoData(self.parent_dir + camera1_folder)
+        console.print(f'Read {len(self.cam1_data.frames)} camera1 frames')
+        # print(self.cam1_timestamp)
+
+        self.cam2_data = VideoData(self.parent_dir + camera2_folder)
+        console.print(f'Read {len(self.cam2_data.frames)} camera2 frames')
+        # print(self.cam2_timestamp)
 
     def load_mocap_mat(self):
         # return the first found mat file
@@ -80,8 +132,8 @@ class DataProcessServer(object):
                       ['Trajectories']['Labeled']['Labels'])
         self.p1_label = (self.p1_mat[mocap_name]
                          ['Trajectories']['Labeled']['Labels'])
-        console.print(self.p1_mat[mocap_name]
-                      ['Trajectories']['Labeled']['Data'][p1_mocap_id, :, :].T.shape)
+        # console.print(self.p1_mat[mocap_name]
+        #               ['Trajectories']['Labeled']['Data'][p1_mocap_id, :, :].T.shape)
         self.p1_df = pd.DataFrame(self.p1_mat[mocap_name]['Trajectories']
                                   ['Labeled']['Data'][p1_mocap_id, 0:3, :].T, columns=['x', 'y', 'z'])
 
@@ -136,15 +188,18 @@ class DataProcessServer(object):
                         ]))
                         ]),
                 dbc.Row([
-                        dbc.Col(html.H3("Hololens hammer point: ", className="btn-secondary"), width=2),
+                        dbc.Col(html.H3("Hololens hammer point: ",
+                                        className="btn-secondary"), width=2),
                         dbc.Col(dcc.Input(id="hololens-hammer-frame", type="number", value=0,
-                                           debounce=True, placeholder="Hammer Frame"), width=2),
-                        dbc.Col(html.H3("Camera1 hammer point: ", className="btn-secondary"), width=2),
+                                          debounce=True, placeholder="Hammer Frame"), width=2),
+                        dbc.Col(html.H3("Camera1 hammer point: ",
+                                        className="btn-secondary"), width=2),
                         dbc.Col(dcc.Input(id="camera1-hammer-frame", type="number", value=0,
-                                           debounce=True, placeholder="Hammer Frame"), width=2),
-                        dbc.Col(html.H3("Camera2 hammer point: ", className="btn-secondary"), width=2),
+                                          debounce=True, placeholder="Hammer Frame"), width=2),
+                        dbc.Col(html.H3("Camera2 hammer point: ",
+                                        className="btn-secondary"), width=2),
                         dbc.Col(dcc.Input(id="camera2-hammer-frame", type="number", value=0,
-                                           debounce=True, placeholder="Hammer Frame"), width=2)]),
+                                          debounce=True, placeholder="Hammer Frame"), width=2)]),
                 dbc.Row([
                     dbc.Col([
                         html.H2(
@@ -166,7 +221,7 @@ class DataProcessServer(object):
                     dbc.Col(html.Button('Prev frame', id='btn-prev',
                                         className='btn btn-primary', n_clicks=0), width=1),
                     dbc.Col(
-                        dcc.Slider(0, len(self.hololens_frames)-1, 1, value=0, marks=None,
+                        dcc.Slider(min=0, max=self.hololens_data.timestamp[-1], step=60, value=0, marks=None,
                                    tooltip={"placement": "bottom",
                                             "always_visible": True},
                                    id='slider-frame',
@@ -183,24 +238,24 @@ class DataProcessServer(object):
             self.frame_idx = slider_value
         elif "btn-next" == ctx.triggered_id:
             self.frame_idx = min(self.frame_idx + 1,
-                                 len(self.hololens_frames) - 1)
+                                 len(self.hololens_data.frames) - 1)
         elif "btn-prev" == ctx.triggered_id:
             self.frame_idx = max(self.frame_idx - 1, 0)
         # elif "interval" == ctx.triggered_id:
         #     self.frame_idx = n_intervals
         fig1 = px.imshow(
-            self.hololens_frames[self.frame_idx  + self.hololens_hammer_point], binary_format="jpg")
+            self.hololens_data.frames[self.hololens_data.get_index(self.frame_idx + self.hololens_hammer_point)], binary_format="jpg")
         fig2 = px.imshow(
-            self.cam1_frames[self.frame_idx + self.camera1_hammer_point], binary_format="jpg")
+            self.cam1_data.frames[self.cam1_data.get_index(self.frame_idx + self.camera1_hammer_point)], binary_format="jpg")
         fig3 = px.imshow(
-            self.cam2_frames[(self.frame_idx + self.camera2_hammer_point)*2], binary_format="jpg")
+            self.cam2_data.frames[self.cam2_data.get_index(self.frame_idx + self.camera2_hammer_point)], binary_format="jpg")
         return fig1, fig2, fig3
 
     def update_plots(self, slider_value):
-        fig = px.line(self.p1_df[self.mocap_hammer_point:(slider_value+1)*6 + self.mocap_hammer_point],
+        fig = px.line(self.p1_df[int(self.mocap_hammer_point/10):int((slider_value + self.mocap_hammer_point) / 10)],
                       labels={'index': 'time (10ms)', 'value': 'pos'}, range_x=[0, 3000])
         return fig
-    
+
     def sync_hammer(self, hololens_hammer_point, camera1_hammer_point, camera2_hammer_point, mocap_hammer_point):
         print("hololens_hammer_point", hololens_hammer_point)
         print("camera1_hammer_point", camera1_hammer_point)
@@ -208,7 +263,7 @@ class DataProcessServer(object):
         print("mocap_hammer_point", mocap_hammer_point)
         self.hololens_hammer_point = hololens_hammer_point
         self.camera1_hammer_point = camera1_hammer_point
-        self.camera2_hammer_point = camera2_hammer_point 
+        self.camera2_hammer_point = camera2_hammer_point
         self.mocap_hammer_point = mocap_hammer_point
         return 0
 
